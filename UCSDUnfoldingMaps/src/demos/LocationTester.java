8f6f9af8f70d272601defa70c40404bde82997ec
Rusty Coder <rusty.coder19@gmail.com>
2017-12-07 13:30:51 -0500

    Added Miami Airport Coordinates

--- UCSDUnfoldingMaps/src/demos/LocationTester.java
+++ UCSDUnfoldingMaps/src/demos/LocationTester.java
@@ -13,9 +13,12 @@ public class LocationTester {
 	{
 		SimpleLocation ucsd = new SimpleLocation(32.9, -117.2);
 		SimpleLocation lima = new SimpleLocation(-12.0, -77.0);
-		
+		SimpleLocation miamiAirport = new SimpleLocation(25.7959, 80.2870);
+
+
 		//latitude = -12.04;
 		System.out.println(ucsd.distance(lima));
+		System.out.println("Miami Airport Coordinates: " + miamiAirport.latitude + ", " + miamiAirport.longitude);
 	}
 
 }
