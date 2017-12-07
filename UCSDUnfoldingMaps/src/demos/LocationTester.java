a360d485d9f5e9f9d3c90d6d541440c22654511c
Rusty Coder <rusty.coder19@gmail.com>
2017-12-07 16:42:02 -0500

    added atlanta airport coordintes

--- UCSDUnfoldingMaps/src/demos/LocationTester.java
+++ UCSDUnfoldingMaps/src/demos/LocationTester.java
@@ -14,6 +14,8 @@ public class LocationTester {
 		SimpleLocation ucsd = new SimpleLocation(32.9, -117.2);
 		SimpleLocation lima = new SimpleLocation(-12.0, -77.0);
 		SimpleLocation miamiAirport = new SimpleLocation(25.7959, 80.2870);
+		SimpleLocation atlantaAirport = new SimpleLocation(33.6407, 84.4277);
+
 
 
 		//latitude = -12.04;
